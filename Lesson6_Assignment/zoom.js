719ed68694d78cbfae22171a4cb651340b383b68
amistick <alm12g@my.fsu.edu>
2015-10-19 17:21:54 +0000

    Lesson 6 Assignment FINAL COMMIT

--- Lesson6_Assignment/zoom.js
+++ Lesson6_Assignment/zoom.js
@@ -1,6 +1,6 @@
 /*  Program Name:  Photo Gallery Application
-    Author: 
-    Date:   
+    Author: Abby Mistick  
+    Date:   October 19, 2015
     Filename: zoom.js
  */
 
@@ -13,7 +13,26 @@ var figFilename = "images/IMG_0" + photoOrderArray[2] + ".jpg";
 /* populate img element and create event listener */
 function pageSetup() {
    document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
+   createEventListeners();
 }
+
+// close window
+function closewin(){
+  window.close();
+  }
+
+
+//create event listener for close button
+function createEventListeners(){
+  var closeWindowDiv = document.getElementsByTagName("p")[0];
+  if (closeWindowDiv.addEventListener){
+    closeWindowDiv.addEventListener("click", closewin, false);
+    } 
+  else if (closeWindowDiv.attachEvent){
+    closeWindowDiv.attachEvent("onclick", closewin);
+  }
+}
+
 
 /* add img src value and create event listener when page finishes loading */
 window.onload = pageSetup;
